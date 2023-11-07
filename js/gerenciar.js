ba77e9deccbf4d00a068e6df6c3d053e5d7344c7
faustinopsy <rodrigohipnose@gmail.com>
2023-11-07 20:45:38 -0300

    modified:   backend/Database/config.php 	modified:   index.html 	modified:   js/gerenciar.js

--- js/gerenciar.js
+++ js/gerenciar.js
@@ -1,21 +1,47 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const token = sessionStorage.getItem('token');
   async function getPerfis() {
       const response = await fetch('backend/Router/PerfilPermissaoRouter.php', {
           method: 'GET',
           headers: {
+              'Authorization':  token,
               'Content-Type': 'application/json'
           }
       });
       return await response.json();
   }
+  async function getListaPermissoes() {
+    const response = await fetch('backend/Router/PerfilPermissaoRouter.php', {
+        method: 'GET',
+        headers: {
+            'Authorization':  token,
+            'Content-Type': 'application/json'
+        }
+    });
+    return await response.json();
+}
+async function populateDropdown() {
+    const permissoes = await getListaPermissoes();
+    const selectElem = document.getElementById("permissaoSelect");
+
+    permissoes.forEach(permissao => {
+        const optionElem = document.createElement("option");
+        optionElem.value = permissao.nome;
+        optionElem.textContent = permissao.nome;
+        selectElem.appendChild(optionElem);
+    });
+}
+
+populateDropdown();
 
   async function addPermissao(perfilId, permissaoName) {
-      const response = await fetch(`backend/Router/PerfilPermissaoRouter.php?perfilId=${perfilId}`, {
+      const response = await fetch(`backend/Router/PerfilPermissaoRouter.php`, {
           method: 'POST',
           headers: {
+               'Authorization':  token,
               'Content-Type': 'application/json'
           },
-          body: JSON.stringify({ nome: permissaoName })
+          body: JSON.stringify({perfilId: perfilId, nome: permissaoName })
       });
       return await response.json();
   }
@@ -24,6 +50,7 @@ document.addEventListener("DOMContentLoaded", function() {
       const response = await fetch(`backend/Router/PerfilPermissaoRouter.php?perfilId=${perfilId}`, {
           method: 'DELETE',
           headers: {
+              'Authorization':  token,
               'Content-Type': 'application/json'
           },
           body: JSON.stringify({ nome: permissaoName })
@@ -35,6 +62,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const response = await fetch(`backend/Router/PerfilPermissaoRouter.php?perfilId=${perfilId}`, {
         method: 'GET',
         headers: {
+            'Authorization':  token,
             'Content-Type': 'application/json'
         }
     });
@@ -66,11 +94,32 @@ document.addEventListener("DOMContentLoaded", function() {
       if (perfilId) {
           const permissoes = await getPermissoes(perfilId);
           if (permissoes && permissoes.length) {
-              permissoes.forEach(permissao => {
-                  const li = document.createElement('li');
-                  li.textContent = permissao.nome;
-                  permissoesList.appendChild(li);
-              });
+            permissoes.forEach(permissao => {
+                const li = document.createElement('li');
+                
+                const span = document.createElement('span');
+                span.textContent = permissao.nome+ " ";
+                li.appendChild(span);
+            
+                const removeBtn = document.createElement('button');
+                removeBtn.textContent = "Remover";
+                removeBtn.classList.add("w3-button")
+                removeBtn.classList.add("w3-round")
+                removeBtn.classList.add("w3-border")
+                removeBtn.addEventListener('click', async function() {
+                    const result = await deletePermissao(perfilId, permissao.nome);
+                    if (result.status) {
+                        alert('Permissão removida com sucesso!');
+                        li.remove(); 
+                    } else {
+                        alert('Erro ao remover permissão.');
+                    }
+                });
+                li.appendChild(removeBtn);
+            
+                permissoesList.appendChild(li);
+            });
+            
           } else {
               const li = document.createElement('li');
               li.textContent = "Sem permissões associadas";
@@ -83,7 +132,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   document.getElementById('addPermissaoBtn').addEventListener('click', async function() {
       const perfilId = document.getElementById('perfilSelect').value;
-      const permissaoName = document.getElementById('permissaoInput').value;
+      const permissaoName = document.getElementById('permissaoSelect').value;
 
       const result = await addPermissao(perfilId, permissaoName);
       if (result.status) {
@@ -94,16 +143,16 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
 
-  document.getElementById('removePermissaoBtn').addEventListener('click', async function() {
-      const perfilId = document.getElementById('perfilSelect').value;
-      const permissaoName = document.getElementById('permissaoInput').value;
+//   document.getElementById('removePermissaoBtn').addEventListener('click', async function() {
+//       const perfilId = document.getElementById('perfilSelect').value;
+//       const permissaoName = document.getElementById('permissaoInput').value;
 
-      const result = await deletePermissao(perfilId, permissaoName);
-      if (result.status) {
-          alert('Permissão removida com sucesso!');
-      } else {
-          alert('Erro ao remover permissão.');
-      }
-  });
+//       const result = await deletePermissao(perfilId, permissaoName);
+//       if (result.status) {
+//           alert('Permissão removida com sucesso!');
+//       } else {
+//           alert('Erro ao remover permissão.');
+//       }
+//   });
  
 });
